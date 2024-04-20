073d14c5be49c0bbb654b9af58367a2589a5e5fa
IsaacNguyen <isaachugh33@gmail.com>
2024-04-20 16:46:22 -0700

    Update main.jsx

--- spotr/src/main.jsx
+++ spotr/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Button from './Button.jsx'
 import './index.css'
-import ParentComponent from './ParentComponent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
